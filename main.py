d5232387a28c5ddc1a1570a0f64197f0bf16eb75
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-02-21 00:16:18 -0800

    refactor: Increase number of events from 100 to 100000

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ tau_p_pion = particle_data_dict['tau_p_child1']
 tau_m_pion = particle_data_dict['tau_m_child1']
 MET = particle_data_dict['MET']
     
-n_events = 100
+n_events = 100000
 
 truth_data = []
 reco_data = []
